0500e584bc1d6fb44d60fde5b1ea0fe92b9d2e33
bertustenhoor <bertustenhoor71@gmail.com>
2022-07-11 13:58:09 +0200

    external data source and upgraded css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { robots } from "./robots";
 import './App.css'
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
-
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             search: '',
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            return response.json();
+        })
+            .then(users => {
+        this.setState({robots: users})
+            });
     }
 
     onSearchChanged = (event) => {
